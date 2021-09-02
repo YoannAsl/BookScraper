ca1590bc1be2b01d8124ec31636790931a161789
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-09-02 18:41:08 +0200

    Now capable of scraping all pages of a category

--- category.py
+++ category.py
@@ -2,48 +2,55 @@ import requests
 from bs4 import BeautifulSoup as soup
 from urllib.parse import urljoin
 
-category_url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
 base_book_url = "http://books.toscrape.com/catalogue"
-
-r = requests.get(category_url)
-
-# HTML parsing
-category_soup = soup(r.content, "html.parser")
 
 # Creates and opens the .csv file
 filename = "category.csv"
-# f = open(filename, "w")
+f = open(filename, "w")
 
 # Headers of the .csv file
 headers = "product_code, product_page_url, title, description, category, image_url, number_available, price_excluding_tax, price_including_tax, review_rating \n"
 
-# f.write(headers)
+f.write(headers)
 
-print("une page") if category_soup.find("ul", class_="pager") == None else print("pages")
+while True:
+    r = requests.get(url)
 
-books = category_soup.find_all("li", class_="col-xs-6")
+    # HTML parsing
+    category_soup = soup(r.content, "html.parser")
 
-for book in books:
-    book_url = urljoin(category_url, book.a["href"])
+    # Gets all books from the page
+    books = category_soup.find_all("li", class_="col-xs-6")
 
-    book_html = requests.get(book_url)
-    book_soup = soup(book_html.content, "html.parser")
+    for book in books:
+        book_url = urljoin(url, book.a["href"])
 
-    # Table that contains a bunch of information we need
-    table = book_soup.find_all("tr")
+        book_html = requests.get(book_url)
+        book_soup = soup(book_html.content, "html.parser")
 
-    product_code = table[0].td.text
-    price_excluding_tax = table[2].td.text
-    price_including_tax = table[3].td.text
-    number_available = table[5].td.text
+        # Table that contains a bunch of information we need
+        table = book_soup.find_all("tr")
 
-    title = book_soup.h1.text
-    description = book_soup.find_all("p")[3].text
-    review_rating = book_soup.find("p", class_="star-rating")["class"][1]
-    category = book_soup.find_all("a")[3].text
+        product_code = table[0].td.text
+        price_excluding_tax = table[2].td.text
+        price_including_tax = table[3].td.text
+        number_available = table[5].td.text
 
-    image_url = urljoin(book_url, book_soup.img["src"])
-#     f.write(
-#         f'{product_code},{book_url},{title.replace(",", "|")},{description.replace(",", "|")},{category},{image_url},{number_available},{price_excluding_tax},{price_including_tax},{review_rating} \n'
-#     )
-# f.close()
+        title = book_soup.h1.text
+        description = book_soup.find_all("p")[3].text
+        review_rating = book_soup.find("p", class_="star-rating")["class"][1]
+        category = book_soup.find_all("a")[3].text
+
+        image_url = urljoin(book_url, book_soup.img["src"])
+        f.write(
+            f'{product_code},{book_url},{title.replace(",", "|")},{description.replace(",", "|")},{category},{image_url},{number_available},{price_excluding_tax},{price_including_tax},{review_rating} \n'
+        )
+    # Selects the Next button, if there is none, closes the csv file and ends the loop
+    next_page_button = category_soup.select_one("li.next > a")
+    if next_page_button:
+        next_page_url = next_page_button.get("href")
+        url = urljoin(url, next_page_url)
+    else:
+        f.close()
+        break
